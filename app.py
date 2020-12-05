942435be9bf8375b9209dba7b5edc7fd77fc0006
harry595 <harry595@naver.com>
2020-12-06 05:29:39 +0900

    mypage/movies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request, redirect, session, send_from_directory, Blueprint, make_response, jsonify, current_app
+from flask import Flask, render_template, json, request,flash, url_for,redirect, session, send_from_directory, Blueprint, make_response, jsonify, current_app
 import pymysql
 import bcrypt
 
@@ -13,17 +13,73 @@ cursor=db.cursor()
 def index():
   return render_template('index.html')
 
-@app.route('/project')
-def project():
-  sql = "SELECT * FROM movie"
-  rows_count = cursor.execute(sql)
-  if rows_count > 0 :
-      movie_info = cursor.fetchall()
-  return render_template('project.html',movies=movie_info)
+@app.route('/movie_queue/<int:movie_id>')
+def movie_queue(movie_id):
+    user_id=session['user']
+    sql = "INSERT INTO moviequeue(movieID,CustomerID) VALUES(%s,%s);"
+    cursor.execute(sql,(movie_id,user_id))
+    db.commit()
+    return redirect('/movies')
 
-@app.route('/components')
-def components():
-  return render_template('components.html')
+@app.route('/movies')
+def movies():
+  rtype=request.args.get('search_type')
+  ktype=request.args.get('keywords')
+  if(ktype == ''):
+    sql = "SELECT * FROM movie"
+    rows_count = cursor.execute(sql)
+    if rows_count > 0 :
+        movie_info = cursor.fetchall()
+    else:
+        movie_info=None
+    return render_template('movies.html',movies=movie_info)
+  else:
+      if(rtype=="movie"):
+        sql = "SELECT * FROM movie WHERE MovieName LIKE %s"
+        rows_count = cursor.execute(sql, (('%' + ktype + '%',)))
+        if rows_count > 0 :
+            movie_info = cursor.fetchall()
+        else:
+            movie_info=None
+        return render_template('movies.html',movies=movie_info)
+      elif(rtype=="actor"):
+        sql = "SELECT * FROM movie left JOIN movieinfo_actor ON movie.MovieID = movieinfo_actor.Movie_info_ID left JOIN actor ON actor.ActorID = movieinfo_actor.Actor_info_ID WHERE ActorName LIKE %s;"
+        rows_count = cursor.execute(sql,(('%' + ktype + '%',)))
+        if rows_count > 0 :
+            movie_info = cursor.fetchall()
+        else:
+            movie_info=None
+        return render_template('movies.html',movies=movie_info)
+      elif(rtype=="movie_type"):
+        sql = "SELECT * FROM MOVIE WHERE MovieType= %s"
+        rows_count = cursor.execute(sql,ktype)
+        if rows_count > 0 :
+            movie_info = cursor.fetchall()
+        else:
+            movie_info=None
+        return render_template('movies.html',movies=movie_info)
+      else:
+        sql = "SELECT * FROM movie"
+        rows_count = cursor.execute(sql)
+        if rows_count > 0 :
+            movie_info = cursor.fetchall()
+        else:
+            movie_info=None
+        return render_template('movies.html',movies=movie_info)
+
+    
+
+@app.route('/mypage')
+def mypage():
+    user_id=session['user']
+    sql = "SELECT * FROM customer WHERE CustomerID= %s"
+    cursor.execute(sql,user_id)
+    user_info = cursor.fetchone()
+
+    sql2 = "SELECT * FROM account where CustomerID=%s"
+    cursor.execute(sql2,user_id)
+    account_info = cursor.fetchall()
+    return render_template('mypage.html',user_info=user_info,account_info=account_info)
 
 @app.route('/logout') 
 def logout(): 
